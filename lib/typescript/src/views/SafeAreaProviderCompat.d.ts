248a510ca67769b7f12da53c32acfeb427c7d530
fedyk <fedyk72@gmail.com>
2020-12-18 17:55:23 +0100

    gitpkg

---
+++ lib/typescript/src/views/SafeAreaProviderCompat.d.ts
@@ -0,0 +1,12 @@
+import * as React from 'react';
+export declare const initialSafeAreaInsets: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+};
+declare type Props = {
+    children: React.ReactNode;
+};
+export default function SafeAreaProviderCompat({ children }: Props): JSX.Element;
+export {};
